93b564d11cc9da5e289f2a25e602090d167ab9c2
pyounuskhan <pyounuskhan@gmail.com>
2017-11-20 20:03:08 +0530

    Comments system

--- resources/views/tickets/show.blade.php
+++ resources/views/tickets/show.blade.php
@@ -37,10 +37,31 @@
                                 </div>
 
                                 <div class="panel panel-body">
-                                     {{ $comment->comment }}     
+                                    {{ $comment->comment }}     
                                 </div>
                             </div>
-                             @endforeach
+                        @endforeach
+                    </div>
+
+                    <div class="comment-form">
+                        <form action="{{ url('comment') }}" method="POST" class="form">
+                            {!! csrf_field() !!}
+
+                            <input type="hidden" name="ticket_id" value="{{ $ticket->id }}">
+
+                            <div class="form-group{{ $errors->has('comment') ? ' has-error' : '' }}">
+                                <textarea rows="10" id="comment" class="form-control" name="comment"></textarea>
+                                @if ($errors->has('comment'))
+                                    <span class="help-block">
+                                        <strong>{{ $errors->first('comment') }}</strong>
+                                    </span>
+                                @endif
+                            </div>
+
+                            <div class="form-group">
+                                <button type="submit" class="btn btn-primary">Submit</button>
+                            </div>
+                        </form>
                     </div>
             </div>
         </div>
