6597b6d330dc49b1f3f613fc7844ccdc2de5a871
pyounuskhan <pyounuskhan@gmail.com>
2017-11-20 18:10:59 +0530

    Second commit

---
+++ resources/views/tickets/show.blade.php
@@ -0,0 +1,48 @@
+@extends('layouts.app')
+
+@section('title', $ticket->title)
+
+@section('content')
+    <div class="row">
+        <div class="col-md-10 col-md-offset-1">
+            <div class="panel panel-default">
+                <div class="panel-heading">
+                    #{{ $ticket->ticket_id }} - {{ $ticket->title }}
+                </div>
+
+                <div class="panel-body">
+                    @include('includes.flash')
+
+                    <div class="ticket-info">
+                        <p>{{ $ticket->message }}</p>
+                        <p>Categry: {{ $category->name }}</p>
+                        <p>
+                        @if ($ticket->status === 'Open')
+                            Status: <span class="label label-success">{{ $ticket->status }}</span>
+                        @else
+                            Status: <span class="label label-danger">{{ $ticket->status }}</span>
+                        @endif
+                        </p>
+                        <p>Created on: {{ $ticket->created_at->diffForHumans() }}</p>
+                    </div>
+
+                    <hr>
+
+                    <div class="comments">
+                        @foreach ($comments as $comment)
+                            <div class="panel panel-@if($ticket->user->id === $comment->user_id) {{"default"}}@else{{"success"}}@endif">
+                                <div class="panel panel-heading">
+                                    {{ $comment->user->name }}
+                                    <span class="pull-right">{{ $comment->created_at->format('Y-m-d') }}</span>
+                                </div>
+
+                                <div class="panel panel-body">
+                                     {{ $comment->comment }}     
+                                </div>
+                            </div>
+                             @endforeach
+                    </div>
+            </div>
+        </div>
+    </div>
+@endsection
